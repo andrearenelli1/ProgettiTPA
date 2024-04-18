744a1782d1884bdba5ab7a4d3e63a4dfbd1a85c4
andreacorradinii <andrea.corradini-3@studenti.unitn.it>
2024-04-18 19:11:04 +0200

    aggiungo la classe posizione se vuoi fare le verifiche

---
+++ posizione.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "classi.h"
+
+using namespace std;
+
+void posizione::print_pos()
+{
+    cout << "(" << get_x() << "," << get_y() << "," << get_orbita() << ")" << endl;
+}
+
+int posizione::get_x()
+{
+    return x;
+}
+
+int posizione::get_y()
+{
+    return y;
+}
+
+int posizione::get_orbita()
+{
+    return orbita;
+}
+
+bool posizione::set_x(int x)
+{
+    //controllo se x è valido:
+    if(-90 < x && x < 90)
+    {
+        this->x = x;
+        return true;
+    }else{
+        this->x = 0;
+        return false;
+    }
+}
+
+bool posizione::set_y(int y)
+{
+    //controllo se x è valido:
+    if(-180 < y && y < 180)
+    {
+        this->y = y;
+        return true;
+    }else
+    {
+        this->y = 0;
+        return false;
+    }
+}
+
+void posizione::set_orbita(int orbita)
+{
+    this->orbita = orbita;
+}
+
+posizione::posizione(int x, int y, int orbita)
+{
+    set_x(x);
+    set_y(y);
+    set_orbita(orbita);
+}
+
+bool posizione::conferma(int x, int y)
+{
+    if(set_x(x) && set_y(y))
+    return true;
+    else
+    return false;
+}

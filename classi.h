776c3be305ba939f0571d00acacc1ae4450a5f35
andrearenelli1 <andrea.renelli@studenti.unitn.it>
2024-04-20 17:03:08 +0200

    aggiunto commenti su costellazione

--- classi.h
+++ classi.h
@@ -54,28 +54,28 @@ class satellite: public posizione
 class costellazione
 {
     private:
-    static int ncos;                    //attributo statico della classe costellazione, viene incrementata dal costruttore, equivale quindi al numero totale di costellazioni create, solo progressivo
-    static int nactive;                 //numero di costellazioni attive (lanciate in orbite geostazionarie)
-    static bool map[181][361][4];       //matrice tridimensionale avente come base tutte le posizioni possibili sul piano considerando solo coordinate intere (181x361), moltiplicate per 4 orbite. La posizione è un bool, 1=occupata, 0=libera. Le orbite sono codificate così 0=30k, 1=35k, 2=36k, 3=37k.
-    int idc;                            //identificatore costellazione (univoco)
-    satellite sat[4];                   //array contenente i 4 satelliti della costellazione     
-    void allineamento(int x, int y);    //allinea i 4 satelliti sugli stessi piani longitudinali e latitudinali
-    void move_n_flag(int orb);          //serve a spostare i satelliti aggiornando la mappa
-    void occ_pos(int x, int y, int orb);    //serve ad occupare una posizione nella mappa     
-    void free_pos(int x, int y, int orb);   //serve a liberare una posizione nella mappa
+    static int ncos;                            //attributo statico della classe costellazione, viene incrementata dal costruttore, equivale quindi al numero totale di costellazioni create, solo progressivo
+    static int nactive;                         //numero di costellazioni attive (lanciate in orbite geostazionarie)
+    static bool map[181][361][4];               //matrice tridimensionale di bool avente come base tutte le posizioni possibili sul piano considerando solo coordinate intere (181x361), moltiplicate per 4 orbite. La posizione è rappresentata come un bool, 1=occupata, 0=libera. Le orbite sono codificate così 0=30k, 1=35k, 2=36k, 3=37k.
+    int idc;                                    //identificatore costellazione (univoco)
+    satellite sat[4];                           //array contenente i 4 satelliti della costellazione     
+    void allineamento(int x, int y);            //allinea i 4 satelliti sugli stessi piani longitudinali e latitudinali con le formule date dal testo del problema, inoltre comunica al satellite di essere posizionate correttamente settando aligned = 1.
+    void move_n_flag(int orb);                  //serve a spostare i satelliti aggiornando la mappa
+    void occ_pos(int x, int y, int orb);        //serve ad occupare una posizione nella mappa     
+    void free_pos(int x, int y, int orb);       //serve a liberare una posizione nella mappa
 
 
     public:
-    costellazione(int x, int y);        //costruttore
-    costellazione();                    //costruttore di default
-    int get_idc();                      //ritorna idc
-    void print_sat();                   //stampa posizioni 4 satelliti
-    bool pos_available(int x, int y, int orb);  //ritorna il valore della mappa per una data posizione
-    bool check(int orb);
-    void lancio();
-    void posizionamento();
-    void erase();
-    int get_nactive();
+    costellazione(int x, int y);                //costruttore
+    costellazione();                            //costruttore di default
+    int get_idc();                              //ritorna identificatore costellazione
+    void print_sat();                           //stampa posizioni 4 satelliti
+    bool pos_available(int x, int y, int orb);  //ritorna il valore della mappa per una data posizione, quindi 0 = libera, 1 = occupata.
+    bool check(int orb);                        //controlla per una data orbita se le coordinate di ogni satellite di sat[4] sono disponibili, ritorna 1 solo se sono disponibili tutte e quattro.
+    void lancio();                              //controlla la disponibilità in orbita 0 per le posizioni di sat[4], in caso positivo cambia l'orbita a 0, altrimenti fallisce con un messaggio di errore.
+    void posizionamento();                      //controlla la disponibilità nelle orbite per il posizionamento e nel caso agisce come descritto dal testo del problema, comunicando a terminale l'esito ed eventuali errori di posizionamento
+    void erase();                               //porta l'orbita a 4, nel caso i satelliti fossero posizionati in orbita, ne aggiorna il numero
+    int get_nactive();                          //ritorna il numero di costellazioni attive
     
 };
 
